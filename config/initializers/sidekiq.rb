892f0d37ea3a1cd19208b14507514badeff9b75b
Ryan Wold <ryan.wold@gsa.gov>
2023-04-11 20:27:26 -0700

    update gems
    * remove redis namespace; use `db` instead

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,15 +5,13 @@ require_relative 'vcap_services'
 Sidekiq.configure_server do |config|
   config.redis = {
     url: ENV.fetch('REDIS_URL', nil),
-    namespace: "touchpoints_sidekiq_#{Rails.env}",
-    ssl_params: {},
+    db: ENV.fetch('REDIS_DB_INSTANCE', 0)
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
     url: ENV.fetch('REDIS_URL', nil),
-    namespace: "touchpoints_sidekiq_#{Rails.env}",
-    ssl_params: {},
+    db: ENV.fetch('REDIS_DB_INSTANCE', 0)
   }
 end
